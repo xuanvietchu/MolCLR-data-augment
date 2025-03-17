56f795e5b989b24b7011c26fe14e2e2248968816
xuanvietchu <vietxuanchu657@gmail.com>
2025-03-16 22:42:51 -0600

    add back edge mask and fix func exc

--- dataset/dataset_func.py
+++ dataset/dataset_func.py
@@ -126,18 +126,38 @@ class MoleculeDataset(Dataset):
             else:
                 mask_nodes_j_expanded.add(node)
         mask_nodes_j = list(mask_nodes_j_expanded)
+        
+        num_mask_edges = max(0, math.floor(0.25 * M))
+        mask_edges_i_single = random.sample(list(range(M)), num_mask_edges)
+        mask_edges_j_single = random.sample(list(range(M)), num_mask_edges)
+        mask_edges_i = [2*i for i in mask_edges_i_single] + [2*i+1 for i in mask_edges_i_single]
+        mask_edges_j = [2*i for i in mask_edges_j_single] + [2*i+1 for i in mask_edges_j_single]
 
         x_i = deepcopy(x)
         for atom_idx in mask_nodes_i:
-            x_i[atom_idx,:] = torch.tensor([len(ATOM_LIST), 0])
-        
-        data_i = Data(x=x_i, edge_index=edge_index, edge_attr=edge_attr)
+            x_i[atom_idx, :] = torch.tensor([len(ATOM_DICT), 0])
+        edge_index_i = torch.zeros((2, 2*(M - num_mask_edges)), dtype=torch.long)
+        edge_attr_i = torch.zeros((2*(M - num_mask_edges), 2), dtype=torch.long)
+        count = 0
+        for bond_idx in range(2 * M):
+            if bond_idx not in mask_edges_i:
+                edge_index_i[:, count] = edge_index[:, bond_idx]
+                edge_attr_i[count, :] = edge_attr[bond_idx, :]
+                count += 1
+        data_i = Data(x=x_i, edge_index=edge_index_i, edge_attr=edge_attr_i)
 
         x_j = deepcopy(x)
         for atom_idx in mask_nodes_j:
-            x_j[atom_idx,:] = torch.tensor([len(ATOM_LIST), 0])
-
-        data_j = Data(x=x_j, edge_index=edge_index, edge_attr=edge_attr)
+            x_j[atom_idx, :] = torch.tensor([len(ATOM_DICT), 0])
+        edge_index_j = torch.zeros((2, 2*(M - num_mask_edges)), dtype=torch.long)
+        edge_attr_j = torch.zeros((2*(M - num_mask_edges), 2), dtype=torch.long)
+        count = 0
+        for bond_idx in range(2 * M):
+            if bond_idx not in mask_edges_j:
+                edge_index_j[:, count] = edge_index[:, bond_idx]
+                edge_attr_j[count, :] = edge_attr[bond_idx, :]
+                count += 1
+        data_j = Data(x=x_j, edge_index=edge_index_j, edge_attr=edge_attr_j)
         
         return data_i, data_j
 
